580ca1290c7e7dadf4763d32a1077b26cf4894c2
GunterPro7 <florianrauscher700@gmail.com>
2024-08-02 11:02:13 +0200

    logger fix

--- src/gunterpro7/logger/logger.py
+++ src/gunterpro7/logger/logger.py
@@ -9,39 +9,65 @@ from src.gunterpro7.utils import file_utils
 _default_color: str = '\033[38m'
 _out_file: str = ""
 
+_old_stdout: object
+_old_stderr: object
+
 
 def __init__():
-    global _out_file
+    global _out_file, _old_stdout, _old_stderr
     _out_folder: str = os.getcwd() + "\\log\\"
     _out_file = _out_folder + "log" + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt"
     os.makedirs(_out_folder, exist_ok=True)
+
+    _old_stdout = sys.stdout
+    _old_stderr = sys.stderr
+
+    sys.stdout = _CustomOutputStream()
+    sys.stderr = _CustomErrorStream()
+
     info("Initializing logger")
 
 
-def success(*message: str) -> None:
-    _print(' '.join(message), 32, _Type.success)
+def success(*message: str, end="\n") -> None:
+    _print(' '.join(message), 32, _Type.success, end)
 
 
-def log(*message: str) -> None:
-    _print(' '.join(message), 38, _Type.log)
+def log(*message: str, end="\n") -> None:
+    _print(' '.join(message), 38, _Type.log, end)
 
 
-def info(*message: str) -> None:
-    _print(' '.join(message), 34, _Type.info)
+def info(*message: str, end="\n") -> None:
+    _print(' '.join(message), 34, _Type.info, end)
 
 
-def warn(*message: str) -> None:
-    _print(' '.join(message), 33, _Type.warn)
+def warn(*message: str, end="\n") -> None:
+    _print(' '.join(message), 33, _Type.warn, end)
 
 
-def error(*message: str) -> None:
-    _print(' '.join(message), 31, _Type.error)
+def error(*message: str, end="\n") -> None:
+    _print(' '.join(message), 31, _Type.error, end)
 
 
-def fatal(*message: str) -> None:
-    _print(' '.join(message), 31, _Type.fatal)
+def fatal(*message: str, end="\n") -> None:
+    _print(' '.join(message), 31, _Type.fatal, end)
     time.sleep(2.5)
     sys.exit(0)
+
+
+class _CustomOutputStream:
+    def write(self, message):
+        log(str(message), end="")
+
+    def flush(self):
+        pass
+
+
+class _CustomErrorStream:
+    def write(self, message):
+        error(str(message), end="")
+
+    def flush(self):
+        pass
 
 
 class _Type(Enum):
@@ -53,11 +79,18 @@ class _Type(Enum):
     fatal = 'fatal'
 
 
-def _print(message: str, color: int, type: _Type) -> None:
+def _print(message: str, color: int, type: _Type, end: str) -> None:
     time_stamp: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    print('\033[' + str(color) + 'm[' + time_stamp + "] " + message + _default_color)
-    file_utils.append_or_create_file(_out_file, "[" + time_stamp + " " + type.name.upper() + "] " + message)
+    prefix: str = "\r" if message.startswith("\r") else ""
+    msg: str = prefix + '\033[' + str(color) + 'm[' + time_stamp + "] " + message.replace("\r", "") + _default_color + end
+    if _old_stderr is not None and _old_stdout is not None:
+        if type == _Type.error:
+            _old_stderr.write(msg)
+        else:
+            _old_stdout.write(msg)
+    file_utils.append_or_create_file(_out_file, "[" + time_stamp + " " + type.name.upper() + "] " +
+                                     message.replace("\r", "").rstrip("\n"))
 
 
 
